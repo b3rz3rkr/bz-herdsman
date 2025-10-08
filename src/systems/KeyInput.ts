22fa978b1b6803bbfdf68320cf376b667f17726d
b3rz3rkr <22635858+b3rz3rkr@users.noreply.github.com>
2025-10-08 09:02:36 +0300

    avoid second KeyInput.init()

--- src/systems/KeyInput.ts
+++ src/systems/KeyInput.ts
@@ -2,8 +2,11 @@ import { Container, Point, Rectangle } from 'pixi.js';
 
 export class KeyInput {
     private static listeners: (({ x, y }: Point) => void)[] = [];
+    private static initialized = false;
 
     static init(stage: Container, screen: Rectangle) {
+        if (this.initialized) return;
+        this.initialized = true;
         stage.eventMode = 'static';
         stage.hitArea = screen;
 
